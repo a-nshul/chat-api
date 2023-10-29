267f667377cbe6bbab1cdcf1d712492996fd6099
Anshul upadhyay <82251676+a-nshul@users.noreply.github.com>
2023-10-29 15:50:00 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,41 +1,3 @@
-// const app=require('express')();
-
-// const server=require('http').createServer(app);
-
-// const io=require('socket.io')(server,{
-//         cors: {
-//           origin: "*",
-//         }
-//       });
-
-// // io.on("connection",(socket)=>{
-// //     socket.on("chat",(payload)=>{
-// //         io.emit("chat",payload);
-// //     });
-// //     socket.on('disconnect', ()=>{
-// //       console.log('disconnected from user');
-// //     });
-// // });
-// io.on("connection", (socket) => {
-//   socket.on("init", (data) => {
-//     socket.join(data.id);
-//     socket.emit("connection", data);
-//   });
-//   socket.on("roomJoined", (room) => {
-//     socket.join(room);
-//   });
-//   socket.on("newMessage", (newMessageRecieved) => {
-//     var chat = newMessageRecieved.chat;
-//     if (!chat.users) return console.log("🚀 ~ Chat.users not defined!");
-//     socket.in(chat.users._id).emit("messageRecieved", newMessageRecieved);
-//   });
-//   socket.on("disconnect", () => {
-//     console.log("User disconnected!");
-//   });
-// });
-// server.listen(4000,()=>{
-//     console.log("server is running on port 4000");
-// })
 const express = require("express");
 const connectDB = require("./config/db");
 const dotenv = require("dotenv");
@@ -49,30 +11,12 @@ dotenv.config();
 connectDB();
 const app = express();
 
-app.use(express.json()); // to accept json data
+app.use(express.json());
 
-// app.get("/", (req, res) => {
-//   res.send("API Running!");
-// });
 
 app.use("/api/user", userRoutes);
 app.use("/api/chat", chatRoutes);
 app.use("/api/message", messageRoutes);
-
-
-// const __dirname1 = path.resolve();
-
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static(path.join(__dirname1, "/frontend/build")));
-
-//   app.get("*", (req, res) =>
-//     res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
-//   );
-// } else {
-//   app.get("/", (req, res) => {
-//     res.send("API is running..");
-//   });
-// }
 
 
 // Error Handling middlewares
@@ -87,42 +31,3 @@ const server = app.listen(
   console.log(`Server running on PORT 3000...`.yellow.bold)
 );
 
-const io = require("socket.io")(server, {
-  pingTimeout: 60000,
-  cors: {
-    origin: "http://localhost:3000",
-    // credentials: true,
-  },
-});
-
-io.on("connection", (socket) => {
-  console.log("Connected to socket.io");
-  socket.on("setup", (userData) => {
-    socket.join(userData._id);
-    socket.emit("connected");
-  });
-
-  socket.on("join chat", (room) => {
-    socket.join(room);
-    console.log("User Joined Room: " + room);
-  });
-  socket.on("typing", (room) => socket.in(room).emit("typing"));
-  socket.on("stop typing", (room) => socket.in(room).emit("stop typing"));
-
-  socket.on("new message", (newMessageRecieved) => {
-    var chat = newMessageRecieved.chat;
-
-    if (!chat.users) return console.log("chat.users not defined");
-
-    chat.users.forEach((user) => {
-      if (user._id == newMessageRecieved.sender._id) return;
-
-      socket.in(user._id).emit("message recieved", newMessageRecieved);
-    });
-  });
-
-  socket.off("setup", () => {
-    console.log("USER DISCONNECTED");
-    socket.leave(userData._id);
-  });
-});
